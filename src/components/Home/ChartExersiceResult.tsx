d3ec23471171bbf7163c54d0fc7796e6ac674fb2
brunocardosof <bruno.cardosof@gmail.com>
2020-12-13 20:23:54 -0300

    add legends to chart exercise result
    add XAxis data to chart exercise result
    
    change data exercise result layout

--- src/components/Home/ChartExersiceResult.tsx
+++ src/components/Home/ChartExersiceResult.tsx
@@ -1,41 +1,118 @@
 import React, {useEffect} from 'react';
-import {AreaChart, Grid, YAxis} from 'react-native-svg-charts';
+import {AreaChart, Grid, XAxis, YAxis} from 'react-native-svg-charts';
 import * as shape from 'd3-shape';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 import {useExercise} from '../../contexts/exercise';
 import {DefaultTheme} from '../../theme';
+import {G, Rect} from 'react-native-svg';
 
 const ChartExersiceResult: React.FC = () => {
   const {currentExercise} = useExercise();
+  const dataXAxis = [0.0, 1.0, 2.0, 3.0, 4.0];
+  const axesSvg = {fontSize: 10, fill: 'grey'};
+  const verticalContentInset = {top: 60, bottom: 10};
+  const xAxisHeight = 15;
 
+  const TooltipTime = () => (
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        marginRight: 5,
+        borderWidth: 1,
+        borderRadius: 10,
+        marginTop: 10,
+        width: 70,
+        height: 30,
+        borderColor: `${DefaultTheme.primaryColor}`,
+        backgroundColor: `${DefaultTheme.primaryColor}`,
+        alignItems: 'center',
+        alignSelf: 'center',
+      }}>
+      <Text style={{textAlign: 'center', color: 'white'}}> 4.7 s</Text>
+    </View>
+  );
+  const TooltipLegend1 = () => (
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        marginLeft: 30,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}>
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          height: 15,
+          width: 15,
+          marginRight: 5,
+          borderWidth: 1,
+          borderColor: `${DefaultTheme.tertiaryColor}`,
+          backgroundColor: `${DefaultTheme.tertiaryColor}`,
+        }}
+      />
+      <Text style={{fontSize: 12}}>Executado (kg)</Text>
+    </View>
+  );
+  const TooltipLegend2 = () => (
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        marginLeft: 30,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}>
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          height: 15,
+          width: 15,
+          marginRight: 5,
+          borderWidth: 1,
+          borderColor: `${DefaultTheme.primaryColor}`,
+          backgroundColor: `${DefaultTheme.primaryColor}`,
+        }}
+      />
+      <Text style={{fontSize: 12}}>Tempo de Contração</Text>
+    </View>
+  );
   return (
-    <View style={{height: 200, flexDirection: 'row'}}>
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        height: 250,
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderColor: `${DefaultTheme.secundaryColor}`,
+      }}>
       <YAxis
         data={currentExercise.dataChart}
-        svg={{
-          fill: 'grey',
-          fontSize: 10,
-        }}
-        contentInset={{top: 30, bottom: 30}}
-        numberOfTicks={3}
-        formatLabel={(value) => `${value}`}
+        style={{marginBottom: xAxisHeight}}
+        numberOfTicks={5}
+        contentInset={verticalContentInset}
+        svg={axesSvg}
       />
-      <AreaChart
-        style={{flex: 1, marginLeft: 16}}
-        data={currentExercise.dataChart}
-        gridMin={1}
-        gridMax={5}
-        contentInset={{top: 30, bottom: 30}}
-        curve={shape.curveBasis}
-        svg={{fill: `${DefaultTheme.primaryColor}`}}>
-        <Grid
-          svg={{
-            strokeMiterlimit: 10,
-          }}
-          direction={Grid.Direction.HORIZONTAL}
+      <View style={{flex: 1, marginHorizontal: 3}}>
+        <AreaChart
+          style={{flex: 1}}
+          data={currentExercise.dataChart}
+          contentInset={verticalContentInset}
+          curve={shape.curveBasis}
+          svg={{fill: `${DefaultTheme.primaryColor}`}}>
+          <Grid />
+          <TooltipLegend1 />
+          <TooltipLegend2 />
+          <TooltipTime />
+        </AreaChart>
+        <XAxis
+          style={{marginHorizontal: -5, height: xAxisHeight}}
+          data={dataXAxis}
+          contentInset={{left: 10, right: 10}}
+          svg={axesSvg}
         />
-      </AreaChart>
+      </View>
     </View>
   );
 };
